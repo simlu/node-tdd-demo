d870e726d021d04beeb6813c238edd0c8e6b5689
Lukas Siemon <lukas.siemon@gmail.com>
2020-02-11 22:10:37 -0800

    chore: added basic external request tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-module.exports = (a, b) => a + b;
+const request = require('request-promise');
+
+module.exports.getExchangeRate = () => request({
+  url: `https://api.exchangerate-api.com/v4/latest/${process.env.CURRENCY}`,
+  json: true
+});
+
+module.exports.getChurchHoliday = () => request({
+  url: `http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${
+    new Date().toISOString().split('T')[0].replace(/-/g, '/')
+  }`,
+  json: true
+});
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,19 @@
 const expect = require('chai').expect;
-const index = require('../src/index');
+const { describe } = require('node-tdd');
+const { getExchangeRate, getChurchHoliday } = require('../src/index');
 
-describe('Testing Package', () => {
-  it('Testing Addition', () => {
-    expect(index(7, 9)).to.equal(16);
+describe('Showcasing node-tdd', {
+  useNock: true,
+  timestamp: 1581485903,
+  cryptoSeed: '0ae8cd8e-84e3-483b-a64a-d169980b4e4a'
+}, () => {
+  it('External exchange rate', async () => {
+    const r = await getExchangeRate();
+    expect(r).to.deep.contain({ base: 'USD' });
+  });
+
+  it('Get Church Holiday', async () => {
+    const r = await getChurchHoliday();
+    expect(r).to.deep.contain({ date: '2020-02-12' });
   });
 });
